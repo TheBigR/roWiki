fa7fdfd67f12124ab58d1c6566285b9bc052dbd1
thebigr <nevoroy@gmail.com>
2018-06-02 22:06:06 +0300

    ngx editor working. need to start cosmetics

---
+++ roWiki/src/app/page-editor/page-editor.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page-editor',
+  templateUrl: './page-editor.component.html',
+  styleUrls: ['./page-editor.component.css']
+})
+export class PageEditorComponent implements OnInit {
+
+  htmlContent: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
