a9e6352b45c736886cec24830eb48a73f2cdb30a
thebigr <nevoroy@gmail.com>
2018-07-05 13:47:26 +0300

    started moving files into root folder.

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'roWiki',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
